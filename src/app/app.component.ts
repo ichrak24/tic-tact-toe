679b9f0f97b7d8cef53336bd606df262e4b0cfe4
ichrak guesmi <guesmiichrak3@gmail.com>
2024-02-28 22:14:03 +0100

    finCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,6 @@
+
+
+
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
@@ -12,326 +15,114 @@ export class AppComponent {
   currentPlayer: number = 0;
   grid: string[][] = [];
   svgContent: SafeHtml = '';
-  turn: number = 0;
   gameResult: string | null = null;
 
   @ViewChild('gridSvg') gridSvg: ElementRef | undefined;
 
   constructor(private sanitizer: DomSanitizer) {}
 
-  
-  /*generateGrid(gridSizeInput: HTMLInputElement) {
-    const size = parseInt(gridSizeInput.value);
-    console.log('Taille de la grille :', size);
-    if (size >= 3) {
-      this.gridSize = size;
-      this.grid = new Array(this.gridSize).fill(null).map(() => new Array(this.gridSize).fill(null));
-      this.currentPlayer = 0;
-  
-      const cellWidth = (100 / this.gridSize) - 5; // Ajuster pour l'épaisseur des lignes
-      const cellHeight = (100 / this.gridSize) - 5;
-      let svgString = '';
-  
-      // Generate rectangles for the grid
-      for (let i = 0; i < this.gridSize; i++) {
-        for (let j = 0; j < this.gridSize; j++) {
-          const x = j * cellWidth;
-          const y = i * cellHeight;
-          svgString += `<rect x="${x}%" y="${y}%" width="${cellWidth}%" height="${cellHeight}%" fill="green"></rect>`;
-        }
-      }
-  
-      const svgContent = `<svg id="grid" class="grid-svg" width="50%" height="50%" viewBox="0 0 100 100">${svgString}</svg>`;
-      this.svgContent = this.sanitizer.bypassSecurityTrustHtml(svgContent);
-         
-      // Sélectionner les rectangles après avoir créé la grille SVG
-      setTimeout(() => {
-        if (this.gridSvg) {
-          const svgElement = this.gridSvg.nativeElement;
-          const rects = svgElement.querySelectorAll('rect');
-          console.log('Rectangles sélectionnés:', rects);
-  
-          rects.forEach((rect: any, index: number) => {
-            console.log('Ajout de l\'événement de clic à un rectangle');
-            rect.addEventListener('click', () => {
-              const row = Math.floor(index / this.gridSize);
-              const col = index % this.gridSize;
-              this.handleCellClick(row, col);
-            });
-          });
-        }
-      }, 0);
-    } 
-  }*/
-
   generateGrid(gridSizeInput: HTMLInputElement) {
     const size = parseInt(gridSizeInput.value);
-    console.log('Taille de la grille :', size);
     if (size >= 3) {
       this.gridSize = size;
-      this.grid = new Array(this.gridSize).fill(null).map(() => new Array(this.gridSize).fill(null));
+      this.grid = Array.from({ length: this.gridSize }, () => Array(this.gridSize).fill(null));
       this.currentPlayer = 0;
-  
-      const cellWidth = (100 / this.gridSize) - 1; // Ajuster pour l'épaisseur des lignes
-      const cellHeight = (100 / this.gridSize) - 1;
-      let svgString = '';
-  
-      // Generate rectangles for the grid
-      for (let i = 0; i < this.gridSize; i++) {
-        for (let j = 0; j < this.gridSize; j++) {
-          const x = j * (100 / this.gridSize);
-          const y = i * (100 / this.gridSize);
-          svgString += `<rect x="${x}%" y="${y}%" width="${cellWidth}%" height="${cellHeight}%" fill="green"></rect>`;
-        }
-      }
-  
-      const svgContent = `<svg id="grid" class="grid-svg" width="50%" height="50%" viewBox="0 0 100 100">${svgString}</svg>`;
-      this.svgContent = this.sanitizer.bypassSecurityTrustHtml(svgContent);
-        
-      // Sélectionner les rectangles après avoir créé la grille SVG
-      setTimeout(() => {
-        if (this.gridSvg) {
-          const svgElement = this.gridSvg.nativeElement;
-          const rects = svgElement.querySelectorAll('rect');
-          console.log('Rectangles sélectionnés:', rects);
-  
-          rects.forEach((rect: any, index: number) => {
-            console.log('Ajout de l\'événement de clic à un rectangle');
-            rect.addEventListener('click', () => {
-              const row = Math.floor(index / this.gridSize);
-              const col = index % this.gridSize;
-              this.handleCellClick(row, col);
-            });
-          });
-        }
-      }, 0);
+      this.updateSVG();
     } 
   }
-  
 
-  
+  handleCellClick(row: number, col: number) {
+    if (this.grid[row][col] === null && !this.gameResult) {
+      this.grid[row][col] = this.symbols[this.currentPlayer];
+      this.currentPlayer = 1 - this.currentPlayer;
+      this.updateSVG();
 
- 
+      if (this.checkWinner()) {
+        this.gameResult = `Le joueur ${this.symbols[1 - this.currentPlayer]} a gagné !`;
+      } else if (this.isDraw()) {
+        this.gameResult = "Match nul !";
+      } else if (this.currentPlayer === 1) {
+        const { row, col } = this.getRandomEmptyCell();
+        if (row !== -1 && col !== -1) {
+          this.grid[row][col] = this.symbols[this.currentPlayer];
+          this.currentPlayer = 1 - this.currentPlayer;
+          this.updateSVG();
 
-
-  ngAfterViewInit() {
-    setTimeout(() => {
-      console.log('Rectangles:');
-      if (this.gridSvg) {
-        const svgElement = this.gridSvg.nativeElement;
-        const rects = svgElement.querySelectorAll('rect');
-        console.log('Rectangles sélectionnés:', rects);
-  
-        rects.forEach((rect: any, index: number) => {
-          console.log('Ajout de l\'événement de clic à un rectangle');
-          rect.addEventListener('click', () => {
-            const row = Math.floor(index / this.gridSize);
-            const col = index % this.gridSize;
-            this.handleCellClick(row, col);
-          });
-        });
-      }
-    }, 0);
-  }
-  
-
-// Ajouter une méthode pour que l'ordinateur sélectionne une cellule disponible de manière aléatoire
-getRandomEmptyCell(): { row: number, col: number } {
-  const emptyCells: { row: number, col: number }[] = [];
-  for (let i = 0; i < this.gridSize; i++) {
-    for (let j = 0; j < this.gridSize; j++) {
-      if (this.grid[i][j] === null) {
-        emptyCells.push({ row: i, col: j });
-      }
-    }
-  }
-  if (emptyCells.length === 0) {
-    return { row: -1, col: -1 }; // Aucune cellule vide disponible
-  }
-  const randomIndex = Math.floor(Math.random() * emptyCells.length);
-  return emptyCells[randomIndex];
-}
-
-  
-  // Modifier la méthode handleCellClick pour inclure le coup de l'ordinateur
-handleCellClick(row: number, col: number) {
-  console.log('Cellule cliquée : ', row, col);
-  if (this.grid[row][col] === null) {
-    this.grid[row][col] = this.symbols[this.currentPlayer]; // Assigner le symbole en fonction du joueur actuel
-    this.currentPlayer = 1 - this.currentPlayer; // Alterner entre les joueurs (0 devient 1, 1 devient 0)
-
-    // Mettre à jour le SVG
-    this.updateSVG(() => {});
-
-    // Vérifier s'il y a un gagnant ou un match nul
-    const winner = this.checkWinner();
-    if (winner) {
-      // Remise de la fonction après l'affichage du résultat
-      setTimeout(() => {
-        if (this.grid.flat().filter(symbol => symbol === winner).length >= 3) {
-          // Afficher le message approprié en fonction du gagnant
-          this.gameResult = winner === "X" ? 'Le joueur X a gagné !' : 'Le joueur O a gagné !';
-          this.resetGame();
-        }
-      }, 0);
-    } else if (this.isDraw()) {
-      this.gameResult = "Match nul !";
-      this.resetGame();
-    } else {
-      // Si toutes les cases sont remplies et qu'aucun gagnant n'a été trouvé
-      // Effacer le résultat précédent s'il y en a un
-      this.gameResult = null;
-
-      // Coup de l'ordinateur
-      const { row, col } = this.getRandomEmptyCell();
-      if (row !== -1 && col !== -1) {
-        this.grid[row][col] = this.symbols[this.currentPlayer];
-        this.currentPlayer = 1 - this.currentPlayer; // Passer au tour du joueur humain
-
-        // Mettre à jour le SVG après le coup de l'ordinateur
-        this.updateSVG(() => {});
-
-        // Vérifier s'il y a un gagnant ou un match nul après le coup de l'ordinateur
-        const computerWinner = this.checkWinner();
-        if (computerWinner) {
-          setTimeout(() => {
-            if (this.grid.flat().filter(symbol => symbol === computerWinner).length >= 3) {
-              this.gameResult = computerWinner === "X" ? 'Le joueur X a gagné !' : 'Le joueur O a gagné !';
-              this.resetGame();
-            }
-          }, 0);
-        } else if (this.isDraw()) {
-          this.gameResult = "Match nul !";
-          this.resetGame();
+          if (this.checkWinner()) {
+            this.gameResult = `Le joueur ${this.symbols[1 - this.currentPlayer]} a gagné !`;
+          } else if (this.isDraw()) {
+            this.gameResult = "Match nul !";
+          }
         }
       }
     }
   }
-}
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
 
-
-
-
-
-
-
-
-
-updateSVG(callback: () => void) {
-  let svgString = '';
-
-  // Draw X or O based on the grid values
-  for (let i = 0; i < this.gridSize; i++) {
-    for (let j = 0; j < this.gridSize; j++) {
-      const cellWidth = 100 / this.gridSize;
-      const cellHeight = 100 / this.gridSize;
-      const xOffset = j * cellWidth + cellWidth / 2;
-      const yOffset = i * cellHeight + cellHeight / 2;
-      const symbol = this.grid[i][j];
-
-      // Dessiner le symbole dans la case s'il existe
-      if (symbol) {
-        svgString += `<text x="${xOffset}%" y="${yOffset}%" dominant-baseline="middle" text-anchor="middle" font-size="5" fill="black">${symbol}</text>`; // Changer la taille de la police à 15
-      }
-    }
-  }
-
-  // Générer le contenu SVG avec le texte ajouté
-  const svgContent = `<svg id="grid" width="100%" height="100%" viewBox="0 0 100 100">${svgString}</svg>`;
-  this.svgContent = this.sanitizer.bypassSecurityTrustHtml(svgContent);
-
-  callback(); // Appel de la fonction de rappel
-}
-
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
-
-
-
-
-  checkWinner(): string | null {
-    // Check rows and columns for winning combinations
-    for (let i = 0; i < this.gridSize; i++) {
-      // Check rows
-      if (
-        this.grid[i][0] === this.grid[i][1] &&
-        this.grid[i][1] === this.grid[i][2] &&
-        this.grid[i][0] !== null
-      ) {
-        return this.grid[i][0];
-      }
-      // Check columns
-      if (
-        this.grid[0][i] === this.grid[1][i] &&
-        this.grid[1][i] === this.grid[2][i] &&
-        this.grid[0][i] !== null
-      ) {
-        return this.grid[0][i];
-      }
-    }
-  
-    // Check diagonals
-    const diagonal1 = this.grid[0][0];
-    const diagonal2 = this.grid[0][2];
-    if (
-      (diagonal1 !== null &&
-        this.grid[1][1] === diagonal1 &&
-        this.grid[2][2] === diagonal1) ||
-      (diagonal2 !== null &&
-        this.grid[1][1] === diagonal2 &&
-        this.grid[2][0] === diagonal2)
-    ) {
-      return this.grid[1][1];
-    }
-  
-    // No winner found
-    return null;
-  }
-  
-  
-  
-  
-
-  isDraw(): boolean {
-    // Check if all cells are filled
+  getRandomEmptyCell(): { row: number, col: number } {
+    const emptyCells: { row: number, col: number }[] = [];
     for (let i = 0; i < this.gridSize; i++) {
       for (let j = 0; j < this.gridSize; j++) {
         if (this.grid[i][j] === null) {
-          return false;
+          emptyCells.push({ row: i, col: j });
+        }
+      }
+    }
+    if (emptyCells.length === 0) {
+      return { row: -1, col: -1 };
+    }
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+    return emptyCells[randomIndex];
+  }
+
+  updateSVG() {
+    let svgString = '';
+    const fontSize = 100 / this.gridSize * 0.5; // Adjust font size based on grid size
+
+    for (let i = 0; i < this.gridSize; i++) {
+      for (let j = 0; j < this.gridSize; j++) {
+        const cellWidth = 100 / this.gridSize;
+        const cellHeight = 100 / this.gridSize;
+        const xOffset = j * cellWidth + cellWidth / 2;
+        const yOffset = i * cellHeight + cellHeight / 2;
+        const symbol = this.grid[i][j];
+
+        if (symbol) {
+          svgString += `<text x="${xOffset}%" y="${yOffset}%" dominant-baseline="middle" text-anchor="middle" font-size="${fontSize}%" fill="black">${symbol}</text>`;
         }
       }
     }
 
-    return true;
+    const svgContent = `<svg id="grid" width="100%" height="100%" viewBox="0 0 100 100">${svgString}</svg>`;
+    this.svgContent = this.sanitizer.bypassSecurityTrustHtml(svgContent);
   }
 
+  checkWinner(): boolean {
+    // Check rows, columns, and diagonals for winning combinations
+    const checkLines = (lines: string[][]): boolean => {
+      return lines.some(line => line.every(symbol => symbol === line[0] && symbol !== null));
+    };
+
+    const lines: string[][] = [...this.grid]; // rows
+    for (let i = 0; i < this.gridSize; i++) {
+      lines.push(this.grid.map(row => row[i])); // columns
+    }
+    lines.push([this.grid[0][0], this.grid[1][1], this.grid[2][2]]); // diagonal
+    lines.push([this.grid[0][2], this.grid[1][1], this.grid[2][0]]); // anti-diagonal
+
+    return checkLines(lines);
+  }
+
+  isDraw(): boolean {
+    return this.grid.every(row => row.every(cell => cell !== null));
+  }
 
   resetGame() {
-    this.grid = new Array(this.gridSize).fill(null).map(() => new Array(this.gridSize).fill(null));
+    this.grid = Array.from({ length: this.gridSize }, () => Array(this.gridSize).fill(null));
     this.currentPlayer = 0;
-    this.turn = 0;
-    this.updateSVG(() => {}); // Mettre à jour le SVG après la réinitialisation
+    this.gameResult = null;
+    this.updateSVG();
   }
-
-
-  
 }
+
+
+
